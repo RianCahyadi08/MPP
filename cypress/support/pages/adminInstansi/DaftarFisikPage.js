accf817798e34bf6ad7da74d68ceb783aed2c68e
LAPTOP-RRMTRDPL\Rian Cahyadi <ryancahyadi11@gmail.com>
2025-12-14 18:24:13 +0700

    test(create)

--- cypress/support/pages/adminInstansi/DaftarFisikPage.js
+++ cypress/support/pages/adminInstansi/DaftarFisikPage.js
@@ -17,6 +17,38 @@ class DaftarFisikPage {
     cy.realPress("Enter");
     cy.wait(500);
   }
+
+  createDataDaerah(description) {
+    cy.get(
+      'a[class*="btn btn-sm btn-icon-white btn-primary btn-text-white hover-scale"]'
+    ).click();
+    cy.wait(500);
+    cy.get(
+      'div[class*="el-select__selected-item el-select__placeholder is-transparent"]'
+    )
+      .eq(0)
+      .click();
+    cy.wait(500);
+    cy.realPress("ArrowDown");
+    cy.wait(500);
+    cy.realPress("Enter");
+    cy.wait(500);
+    // Pilih daerah
+    cy.realPress("Tab");
+    cy.wait(500);
+    cy.realPress("Enter");
+    cy.wait(500);
+    cy.realPress("ArrowDown");
+    cy.wait(500);
+    cy.realPress("Enter");
+    // input deskripsi
+    cy.get('textarea[placeholder*="Please input"]')
+      .type(description)
+      .should("have.value", description);
+    cy.wait(500);
+    cy.get('button[class*="btn btn-primary"]').click();
+    cy.wait(500);
+  }
 }
 
 module.exports = new DaftarFisikPage();
