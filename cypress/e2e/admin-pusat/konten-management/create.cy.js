29c37702be4975d54a651a1657b821fe91405f68
LAPTOP-RRMTRDPL\Rian Cahyadi <ryancahyadi11@gmail.com>
2025-12-14 18:25:28 +0700

    update(create)

--- cypress/e2e/admin-pusat/konten-management/create.cy.js
+++ cypress/e2e/admin-pusat/konten-management/create.cy.js
@@ -1,14 +1,14 @@
 import DaftarFisikPage from "../../../support/pages/adminInstansi/DaftarFisikPage";
 import LoginPage from "../../../support/pages/LoginPage";
 
-describe("SC01 - View Page Daftar Fisik", () => {
+describe("SC01 - Create Data Daftar Fisik", () => {
   const env = Cypress.env("stageAdmin");
 
   beforeEach(() => {
     cy.visit(env);
   });
 
-  it("TC01 - View Page Daftar Fisik", () => {
+  it("TC01 - Create Data Daftar Fisik", () => {
     cy.fixture("users").then((data) => {
       LoginPage.loginAdmin(data.admin.email, data.admin.password);
       DaftarFisikPage.accessDaftarFisikPage();
